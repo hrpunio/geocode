7b3b9b2a8d678e0620fbe50f05edab8f9c12930e
tomasz przechlewski <rudolf.von.ems@gmail.com>
2016-03-08 18:44:56 +0100

    pomorskie/zach.pomorskie okr. wyborcze

--- README.md
+++ README.md
@@ -20,3 +20,38 @@ perl sum.pl urzedowy_wykaz_nazw_miejscowosci_2015.csv | grep '==='  | awkF '{pri
 grep ";wieś\|miasto;" urzedowy_wykaz_nazw_miejscowosci_2015.csv | awkF 'BEGIN {OFS=";"}; \
    {print $1, $2, $3, $4, $5, $6 }' > nazwy_pl.csv
 
+
+Wypisz pomorskie i zachodnio-pomorskie adresy okręgów wyborczych
+----------------------------------------------------------------
+
+awk -F ';' '$2 ~ /^22/ {print $0}' kody_adresy_poprawiony_Coords.csv  | wc -l
+$1453
+
+# ręcznie usuwamy statki tj.:
+# 103138;229801;Baltica 103139;229801;Le Quy Don 103140;229901;Oceania
+# 103141;229901;Baltic Beta 103142;229901;LOTOS Petrobaltic
+
+# zachodnio-pomorskie 
+awk -F ';' '$2 ~ /^32/ {print $0}' kody_adresy_poprawiony_Coords.csv >> kody_adresy_poprawiony_Coords_Pomorskie.csv
+
+wc -l kody_adresy_poprawiony_Coords_Pomorskie.csv 
+$2574 kody_adresy_poprawiony_Coords_Pomorskie.csv
+
+# adresy zdublowane kończą się na CX;CX;CX
+grep -v 'CX;CX' kody_adresy_poprawiony_Coords_BB_Pomorskie.csv | wc -l
+$2302
+
+grep -v 'CX;CX' kody_adresy_poprawiony_Coords_BB_Pomorskie.csv | grep ROOFTOP | wc -l
+$1858  ## 80,71%
+
+## Kompletnie bez sensu (wskazanie Poland jako obszaru)
+awk -F ';'  '{ print $(NF-1) }' kody_adresy_poprawiony_Coords_BB_Pomorskie.csv | grep "^Poland$" | wc -l
+$210 ## 9,12%
+
+# lista dziwnych adresów
+grep -v 'CX;CX;' kody_adresy_poprawiony_Coords_BB_Pomorskie.csv | awk -F ';'  '$(NF-1) == "Poland" { print $0 }'
+
+# liczba dziwnych adresów
+grep -v 'CX;CX;' kody_adresy_poprawiony_Coords_BB_Pomorskie.csv | awk -F ';'  '$(NF-1) == "Poland" { print $0 }' | wc -l
+$210
+
--- geocodeCoder0.pl
+++ geocodeCoder0.pl
@@ -5,6 +5,8 @@ use strict;
 use LWP::Simple; # from CPAN
 use JSON qw( decode_json ); # from CPAN
 use Getopt::Long;
+use Encode;
+##use utf8;
 
 my $format = "json"; #can also to 'xml'
 my $geocodeapi = "https://maps.googleapis.com/maps/api/geocode/";
@@ -27,7 +29,7 @@ my $USAGE="USAGE: $0 [-a ADDRESS] [-p POSTCODE] [-log FILENAME] [-bb SWlat,SWlng
 GetOptions( "a=s" => \$addrLine, "p=s" => \$postalCode, "log=s" => \$logFile, "bb=s" => \$boundingBox,
     "country=s" => \$country, "aa1=s" => \$aArea1, "aa2=s" => \$aArea2, "dryrun" => \$dryModeRun, ) ;
 
-if ($logFile) { open LOG, ">$logFile" || die "Cannot log to $logFile\n" ;
+if ($logFile) { open LOG, ">>$logFile" || die "Cannot log to $logFile\n" ;
  print STDERR "*** Logging JSON to $logFile ***\n" ; } else {
  print STDERR "*** Logging JSON OFF ***\n"; }
 
@@ -36,8 +38,11 @@ unless ($addrLine || $postalCode || $aArea1 ) { print STDERR "$USAGE"; exit 0; }
 my @res = getLatLong("$addrLine", "$postalCode");
 my $coords = "@res[0,1]";
 my $bbox = "@res[2,3,4,5]";
+my $accuracy = "@res[6]";
+my $fa = "@res[7]";
+my $addr_types = "@res[8]";
 
-print "Lat/Lng: $coords Bbox: $bbox\n";
+print "Lat/Lng: $coords Bbox: $bbox [$accuracy | $fa | $addr_types ]\n";
 
 if ($logFile) { close (LOG) ; }
 
@@ -59,19 +64,19 @@ sub getLatLong($){
   # jeżeli adres jest '' to &address= będzie pominięte:
   # ?components=country:PL|postal_code:81-825
   if ($address) { $url .= "&address=" . $address; $comment .= "$address" ; }
-  if ($boundingBox) { $url .= "&bounds=$boundingBox" . $address; }
+  if ($boundingBox) { $url .= "&bounds=$boundingBox" ; }
 
   if ( $addrLine ) { print STDERR "*** Address: $address [$country] *** "}
   if ( $postalCode ) { print STDERR "*** PostalCode: $postal [$country] *** "  ; $comment .= "|$postal"; }
   if ( $aArea1 ) { print STDERR "*** Adm.Area.1: $aArea1 [$country] *** " ; $comment .= "|$aArea1" ; }
   if ( $aArea2 ) { print STDERR "*** Adm.Area.2: $aArea2 [$country] *** " ; $comment .= "|$aArea2" ; }
   
-  print STDERR "\n*** [ $url ] ***\n";
+  print STDERR "\n*** [URL: $url  ] ***\n";
   if ($dryModeRun) { exit } ## nie marnuj limitu *--*--*
   
   my $json = get($url);
 
-  if ($logFile) { print LOG "{ _comment_: \"$comment\" }\n$json\n"; }
+  if ($logFile) { print LOG "##_comment_: $comment\n##_comment_:$url\n$json\n"; }
 
   my $d_json = decode_json( $json );
 
@@ -85,7 +90,15 @@ sub getLatLong($){
     my $boundsSWlat = $d_json->{results}->[0]->{geometry}->{bounds}->{southwest}->{lat};
     my $boundsSWlon = $d_json->{results}->[0]->{geometry}->{bounds}->{southwest}->{lng};
 
-    return ($lat, $lng, $boundsNElat, $boundsNElon, $boundsSWlat, $boundsSWlon, );
+    my $loc_type = $d_json->{results}->[0]->{geometry}->{location_type};
+    ## UTF problem
+    my $formatted_address = $d_json->{results}->[0]->{formatted_address};
+    ## nie mam pojęcia czemu ale encode jest potrzebne ##:
+    $formatted_address = encode("utf-8", $formatted_address);
+
+    my $addr_t = $d_json->{results}->[0]->{types}->[0] . "/" . $d_json->{results}->[0]->{types}->[1];
+
+    return ($lat, $lng, $boundsNElat, $boundsNElon, $boundsSWlat, $boundsSWlon, $loc_type, $formatted_address, $addr_t );
   } else {
     return ( $d_json->{status} )
   }
